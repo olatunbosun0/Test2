6e39f184217bce181563814ffd68624233c84274
olatunbosun0 <143437842+olatunbosun0@users.noreply.github.com>
2023-09-05 16:17:25 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+ HEAD
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
 =======
 This ia another commit
->>>>>>> 01c510da44e303a603a5c51f59cb60e386e45699
+ 01c510da44e303a603a5c51f59cb60e386e45699
